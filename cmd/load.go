76febad928897f36d5d500a203f4020fc32cdee6
agrabauskas <agrabauskas@palantir.com>
2020-04-09 18:59:18 +0100

    implement load save

--- cmd/load.go
+++ cmd/load.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/url"
+
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
 
@@ -10,24 +15,55 @@ func init() {
 
 var gitRemoteCmd = &cobra.Command{
 	Use:   "load",
-	Args:  cobra.NoArgs,
+	Args:  cobra.ExactArgs(1),
 	Short: "load perviously saved query arguments",
 	Long:  "finds and loads previously saved query arguments",
 	RunE:  loadQuery,
 	// PersistentPostRunE: focusBrowser,
 }
 
-func loadQuery(_ *cobra.Command, _ []string) error {
-	// s, err := git.GetRoot()
-	// if err != nil {
-	// 	return err
-	// }
-	// ok, err := tabNavigaotr.Focus(s)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !ok {
-	// 	tabNavigaotr.Open(s)
-	// }
+func loadQuery(_ *cobra.Command, args []string) error {
+	inputURL, err := url.Parse(args[0])
+	if err != nil {
+		return err
+	}
+
+	dir, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+	filedir := dir + "/.cache/uqc/"
+
+	fileBytes, err := ioutil.ReadFile(filedir + inputURL.Host)
+	if err != nil {
+		return err
+	}
+
+	savedURL, err := inputURL.Parse(string(fileBytes))
+	if err != nil {
+		return err
+	}
+
+	queryValues := inputURL.Query()
+	for key, valArr := range savedURL.Query() {
+		queryValues.Del(key)
+		for _, val := range valArr {
+			queryValues.Add(key, val)
+		}
+	}
+
+	outputURL := url.URL{
+		Scheme:     inputURL.Scheme,
+		Opaque:     inputURL.Opaque,
+		User:       inputURL.User,
+		Host:       inputURL.Host,
+		Path:       inputURL.Path,
+		RawPath:    inputURL.RawPath,
+		ForceQuery: inputURL.ForceQuery,
+		RawQuery:	queryValues.Encode(),
+		Fragment:   inputURL.Fragment,
+	}
+
+	fmt.Println(outputURL.String())
 	return nil
 }
--- cmd/save.go
+++ cmd/save.go
@@ -1,7 +1,16 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"net/url"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+)
+
+const (
+	linuxSecurityMode = 0777
 )
 
 func init() {
@@ -10,24 +19,37 @@ func init() {
 
 var saveCmd = &cobra.Command{
 	Use:   "save",
-	Args:  cobra.NoArgs,
+	Args:  cobra.ExactArgs(1),
 	Short: "save url query",
 	Long:  "extracts query arguments from url and saves them into a file",
 	RunE:  saveQuery,
 	//PersistentPostRunE: focusBrowser,
 }
 
-func saveQuery(_ *cobra.Command, _ []string) error {
-	// s, err := git.GetRoot()
-	// if err != nil {
-	// 	return err
-	// }
-	// ok, err := tabNavigaotr.Focus(s)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !ok {
-	// 	tabNavigaotr.Open(s)
-	// }
+func saveQuery(_ *cobra.Command, args []string) error {
+	url, err := url.Parse(args[0])
+	if err != nil {
+		return err
+	}
+
+	dir, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+	filedir := dir + "/.cache/uqc/"
+
+	_, err = os.Stat(filedir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			os.Mkdir(filedir, linuxSecurityMode)
+		} else {
+			return err
+		}
+	}
+
+	err = ioutil.WriteFile(filedir+url.Host, []byte(args[0]), linuxSecurityMode)
+	if err != nil {
+		return err
+	}
 	return nil
 }
